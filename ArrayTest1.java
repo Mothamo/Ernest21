977452f9ce42fe1357d3ece3f74a72314693a60b
Mothamo <94404697+Mothamo@users.noreply.github.com>
2022-01-06 21:09:26 +0200

    Add files via upload

---
+++ ArrayTest1.java
@@ -0,0 +1,54 @@
+
+public class  SortArrayAsc
+ {
+
+    public static void main(String [] ar){
+        int i=0;
+        //String myString="8,2,3,4";
+        String line = "1,4,3,2,3,7,5,6,9"; 
+
+        String [] array=line.split(",");
+         int [] myarray=new int[array.length];
+
+
+        for(String a:array)
+        {
+            myarray[i] =Integer.parseInt(a);
+
+             i++;
+        }
+
+        returnSoterdArray(myarray);
+
+
+
+    }
+
+
+    private  static void returnSoterdArray(int [] array){
+
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = i + 1; j < array.length; j++)
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+
+
+
+                    }
+     for(int a:array) System.out.println(a+" ");
+
+
+
+
+                
+    
+    }
+
+
+
+        }
+
